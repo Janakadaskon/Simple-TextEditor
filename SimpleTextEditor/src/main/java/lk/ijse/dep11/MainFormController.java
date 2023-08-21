d89e53af4f2e918de513211a56672cc77b69d921
Janaka Daskon <janakadaskon@gmail.com>
2023-08-22 01:31:10 +0530

    Create About us

--- SimpleTextEditor/src/main/java/lk/ijse/dep11/MainFormController.java
+++ SimpleTextEditor/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -1,14 +1,31 @@
 package lk.ijse.dep11;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class MainFormController {
     public MenuItem mnitemUserGuide;
     public MenuItem mnitemAboutUs;
+    public AnchorPane root;
 
-    public void mnitemUserGuideOnAction(ActionEvent actionEvent) {
-        
+    public void mnitemUserGuideOnAction(ActionEvent actionEvent)throws Exception {
+        AnchorPane scene1Root = FXMLLoader.load(getClass().getResource("/view/Scene1.fxml"));
+        Scene mainScene = new Scene(scene1Root);
+
+        Stage stage = new Stage();
+
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(root.getScene().getWindow());
+
+        stage.setScene(mainScene);
+        stage.setTitle("User Guide");
+        stage.show();
+
 
     }
 
