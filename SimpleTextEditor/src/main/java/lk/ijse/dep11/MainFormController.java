0b082c9436395c77680fb8f322f922055b9e35ea
Janaka Daskon <janakadaskon@gmail.com>
2023-08-22 01:16:10 +0530

    Create Scene1 & Scene2 windows

--- SimpleTextEditor/src/main/java/lk/ijse/dep11/MainFormController.java
+++ SimpleTextEditor/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -1,4 +1,17 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+
 public class MainFormController {
+    public MenuItem mnitemUserGuide;
+    public MenuItem mnitemAboutUs;
+
+    public void mnitemUserGuideOnAction(ActionEvent actionEvent) {
+        
+
+    }
+
+    public void mnitemAboutUsOnAction(ActionEvent actionEvent) {
+    }
 }
