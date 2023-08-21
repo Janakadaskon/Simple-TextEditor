5953ce141329735e4a130836552f72c2a7a4012b
Janaka Daskon <janakadaskon@gmail.com>
2023-08-21 23:49:54 +0530

    Add Appinitializer & AppinitializerWrapper

---
+++ SimpleTextEditor/src/main/java/lk/ijse/dep11/AppinitializerWrapper.java
@@ -0,0 +1,12 @@
+package lk.ijse.dep11;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppinitializerWrapper  {
+
+    public static void main(String[] args) {
+        Appinitializer.main(args);
+
+    }
+}
